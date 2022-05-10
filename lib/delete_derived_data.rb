974c05e63d01f0184b63714675d6242fd04d7ede
Nicolae Rogojan <rogojancolea@gmail.com>
2022-05-10 21:22:22 +0300

    add delete paths

--- lib/delete_derived_data.rb
+++ lib/delete_derived_data.rb
@@ -10,22 +10,11 @@ module DeleteDerivedData
     archives_paths = search.getArchives if options[:archives]
 
     if !derived_paths.nil? && derived_paths.length > 0
-      puts "derived_paths"
-      # Delete.run(derived_paths, options)
+      Delete.run(derived_paths, options)
     end
 
     if !archives_paths.nil? && archives_paths.length > 0
-      puts "archives_paths"
-      # Delete.run(archives_paths, options)
+      Delete.run(archives_paths, options)
     end
-  end
-
-  def self.have_full_disk_access
-    cmd = "$(sw_vers -productVersion | cut -d . -f 1) -lt 11 ] && [ $(sw_vers -productVersion | cut -d . -f 2) -lt 15"
-    system( cmd )
-    
-    cmd = "sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access' > /dev/null 2>&1 && true || false"
-    wasGood = system( cmd )
-    puts wasGood
   end
 end
